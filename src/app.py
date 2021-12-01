92ba7487b3451bc3ae59acc8fed6d20a1385498d
sarahbejerano <bejeranosarah@gmail.com>
2021-12-01 23:05:37 +0000

    creating a Rest API

--- src/app.py
+++ src/app.py
@@ -1,58 +1,19 @@
-from flask import Flask,jsonify
-from flask import request
-
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+from metmuseum import objects
 
-objects = [
-    { "objects": "My first object", "objectID": True },
-    { "objects": "My second object", "objectID": True }
-]
+# creando una ruta, es decir que cuando se visite la ruta /objects se va a ejecutar la funcion.
+# queremos que retorne en Jsonify (formato de intercambio de datos ideal). desde la ruta decidimos tb que tipo de metodo utilizamos 
 
-
-@app.route('/objects', methods=['GET'])
-def get_objects():
+@app.route('/objects', methods=["GET"])
+def getobjects():
   return jsonify(objects)
 
 
-# @app.route('/objects', methods=['POST'])
-# def add_new_object():
-#     request_body = request.get_json(force=True)
-#     objects.append(request_body)
-#     return jsonify(objects)
-
-
-
-
-
 
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
-
-# @app.route('/blabla', methods=['GET'])
-# def hello_world():
-#     return 'Hello, World!'
-
-
-# # def hello_world():
-#     # supongamos que tienes some_data (cierta información) en una variable json
-#     some_data = { "name": "Bobby", "lastname": "Rixer" }
-
-#     # puedes convertir esa variable en un string json así
-#     json_text = flask.jsonify(some_data)
-
-#     # y luego puedes retornarla (return) en el response body así:
-#     return json_text
-
-# @app.route('/todos', methods=['POST'])
-# def add_new_todo():
-#     request_body = request.data
-#     print("Incoming request with the following body", request_body)
-#     return 'Response for the POST todo'
-
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3000, debug=True)
---
+++ src/metmuseum.py
@@ -0,0 +1,18 @@
+objects = [
+    {"objectID": 732804,
+    "title":"The Sentinels. C. Grove",
+    "primaryImage": "https://images.metmuseum.org/CRDImages/ph/original/DP136810.jpg",
+    "objectDate": 1878},
+
+    {"objectID": 287289,
+    "title":"Spiral of Lightning in a Thunderstorm",
+    "primaryImage":"https://images.metmuseum.org/CRDImages/ph/original/DP156451.jpg",
+    "objectDate": 1886},
+
+
+    {"objectID":283210,
+    "title":"Ti ra wa hut Re sa ru",
+    "primaryImage":"https://images.metmuseum.org/CRDImages/ph/original/DP137202.jpg",
+    "objectDate": 1867},
+
+]
